285908d1461a48944314d2f3951cb06e429c5ee4
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2024-01-23 20:50:51 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BarraLateralComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
